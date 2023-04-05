2a7bfa675a48616e62270f35f1f67774dfbc64ef
Luiz Fernando <62849389+luiz826@users.noreply.github.com>
2023-04-05 12:05:03 -0300

    Creating vectorized horizontal_simetry

--- src/utils.py
+++ src/utils.py
@@ -11,55 +11,34 @@ def intensity(pixels: np.array) -> np.array:
 
 # Simetria
 
-def vertical_simetry(img):
-    p = img.reshape(
-        N_PIXELS, 
-        N_PIXELS
-    )
+def vertical_simetry(pixels):
+    '''
+      pixels -> Is all the dataset
+    '''
+    p = pixels.reshape(-1, 28, 28)
 
-    sum_diffs = 0
-    for k in range(N_PIXELS):
-        sum_diffs_row = 0 
-        for i in range((N_PIXELS-2)//2): # 13
-            j = (N_PIXELS-1)-i
-            sum_diffs_row += abs(p[k][i] - p[k][j])
+    A = p[:, :, :14]
+    B = p[:, :, 14:]
 
-        sum_diffs += sum_diffs_row
+    B_ = np.flip(B, 2)
 
-    return sum_diffs / MAX_TON
+    return np.sum(np.abs(A - B_), axis=1) / 255
 
-def horizontal_simetry(img):
-    p = img.reshape(
-        N_PIXELS, 
-        N_PIXELS
-    )
-    
-    sum_diffs = 0
-    for k in range((N_PIXELS-2)//2):
-        sum_diffs_col = 0 
-        for i in range(N_PIXELS):
-            j = 27-k
-            sum_diffs_col += abs(p[k][i] - p[j][i])
+def horizontal_simetry(pixels):
+    '''
+    pixels -> Is all the dataset
+    '''
+    p = pixels.reshape(-1, 28, 28)
 
-        sum_diffs += sum_diffs_col
-    
-    return sum_diffs / MAX_TON
+    A = p[:, :14, :]
+    B = p[:, 14:, :]
+
+    B_ = np.flip(B, 1)
+
+    return np.sum(np.abs(A - B_), axis=1) / 255
 
 def simetry(pixels):
-    ver = np.array(list(map(vertical_simetry, pixels))) 
-    hor = np.array(list(map(horizontal_simetry, pixels)))
+    ver = vertical_simetry, pixels)
+    hor = horizontal_simetry, pixels)
     
     return ver + hor
-
-def vertical_simetry2(pixels):
-  '''
-    pixels -> Is all the dataset
-  '''
-  p = pixels.reshape(-1, 28, 28)
-
-  A = p[:, :, :14]
-  B = p[:, :, 14:]
-
-  B_ = np.flip(B, 2)
-
-  return np.sum(np.abs(A - B_), axis=1) / 255
