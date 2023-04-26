3bc6a916173fd88ecdb20a149fd2d652d7f2b442
Luiz Costa <luiz.costa@nubank.com.br>
2023-04-25 22:37:49 -0300

    adjusting 1x5

--- src/classifier_1x5_utils.py
+++ src/classifier_1x5_utils.py
@@ -11,18 +11,19 @@ def filter_1x5(df: pd.DataFrame, or_ = True) -> pd.DataFrame:
     
     return df[filter_1], df[filter_5]
 
-def plot1x5(df: pd.DataFrame, wei = []) -> None:
+def plot1x5(df: pd.DataFrame, ax=plt, wei = []) -> None:
     filter_1, filter_5 = filter_1x5(df, False)
     
     if len(wei) != 0:
         x1 = np.array([20, 150]) # Valores arbitrários de x1 para desenhar a linha
         x2 = -(wei[0] + wei[1]*x1) / wei[2]
-        plt.plot(x1, x2)
+        ax.plot(x1, x2)
 
-    plt.scatter(filter_1['intensidade'], filter_1['simetria'], color="blue", label="Um")
-    plt.scatter(filter_5['intensidade'], filter_5['simetria'], color="red", label="Cinco")
-    plt.ylabel("Simetria")
-    plt.xlabel("Intensidade")
-    plt.title("Intensidade x Simetria")
-    plt.legend()
+    ax.scatter(filter_1['intensidade'], filter_1['simetria'], color="blue", label="Um")
+    ax.scatter(filter_5['intensidade'], filter_5['simetria'], color="red", label="Cinco")
+    ax.ylabel("Simetria")
+    ax.set_xlabel("Intensidade")
+    ax.set_ylim(20, 200)
+    ax.set_title("Intensidade x Simetria")
+    ax.set_legend()
     plt.show()
